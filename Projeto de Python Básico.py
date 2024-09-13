14e48773c7cb07fd879213da267548c713d58f50
michaeldouglasx <michael_douglasfs@hotmail.com>
2024-09-13 14:34:47 -0300

    commit "Projeto de Python B\303\241sico.py"

---
+++ Projeto de Python Básico.py
@@ -0,0 +1,9 @@
+idade = int (input("Qual a sua idade? "))
+if idade >= 18:
+    print ("Você é maior de idade! ")
+else:
+    print("Você é menor de idade! ")
+if idade >= 18:
+    print("Você pode tirar a carteira de motorista!")
+else:
+    print("Você não possui idade suficiente para tirar a carteira de motorista!")
