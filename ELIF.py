dc6d6e3b8e842feb6e625e1c39d8cbd5d15a1caf
michaeldouglasx <michael_douglasfs@hotmail.com>
2024-09-14 18:19:40 -0300

    commit

--- ELIF.py
+++
@@ -1,9 +0,0 @@
-idade = int(input("Digite sua idade: "))
-if idade < 10:
-    print ("Você é uma criança! ")
-elif 11 <= idade < 18:
-    print("Você é um(a) pré adolescente! ") 
-elif 18 <= idade < 65:
-    print("Você é um adulto! ")
-else:
-    print("Você é um idoso! ")
--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-
-#Programa para descobrir se eu torço para o maior do brasil
-time = "Palmeiras"
-meutime = input("Qual seu time?:")
-if meutime == "Palmeiras":
- print ("Você torce para o maior time do Brasil!", "Parabéns")
-else:
- print("Você torce para um time pequeno! ")
- 
