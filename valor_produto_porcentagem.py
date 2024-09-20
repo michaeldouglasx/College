800e10547b09157f20ad67934b1cf4b315c6e0f2
Michael Douglas <michael_douglasfs@hotmail.com>
2024-09-20 14:01:06 -0300

    Add files via upload

---
+++ valor_produto_porcentagem.py
@@ -0,0 +1,8 @@
+entrada =input("digite os valores dos produtos utilizando virgula: ")
+
+valor_produto = [float(valor) for valor in entrada.split(",")]
+acrescimo = float(input("Qual a porcentagem desejada?")) /100
+
+for preco in valor_produto:
+    novo_preco = preco * (1 + acrescimo)
+    print (novo_preco)
