640683dcba99d320efaf89b04308bee54d838b4e
michaeldouglasx <michael_douglasfs@hotmail.com>
2024-10-02 14:32:56 -0300

    Raíz quadrada

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,7 @@
-print("ola mundo")
+import math
+num = int(input("Digite um número: "))
+num2 = int(input("Digite um número: "))
+raiz = math.sqrt (num)
+raiz2 = math.sqrt (num2)
+print (f"A raíz de {num} é: {raiz}")
+print (f"A raíz de {num2} é: {raiz2}")
