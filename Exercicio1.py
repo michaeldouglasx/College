760b01f0d03cdc9ff0f1b472b805239dc423ae8b
michaeldouglasx <michael_douglasfs@hotmail.com>
2024-10-16 13:51:37 -0300

    new file:   Exercicio1.py

---
+++ Exercicio1.py
@@ -0,0 +1,8 @@
+import math
+numero = int(input("Digite um número: "))
+dobro = numero * 2
+triplo = numero * 3
+raizquadrada = math.sqrt(numero)
+print (f"O dobro do número informado é: {dobro}")
+print (f"O triplo é: {triplo}")
+print (f"A raíz quadrada é: {raizquadrada}  ")
